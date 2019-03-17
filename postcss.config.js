4aa4603ea0d19ea47ca8484ab91d105e259feb47
Didi Medina <hi@didimedina.com>
2019-03-17 14:21:42 +0200

    first commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,18 @@
-var tailwindcss = require('tailwindcss');
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
+  modules: true,
   plugins: [
-    tailwindcss('./tailwind.js')
-  ]
+    tailwindcss('./tailwind.js'),
+    cssnano({
+      preset: 'default',
+    }),
+    purgecss({
+      content: ['./**/*.html'],
+    }),
+    autoprefixer,
+  ],
 };
