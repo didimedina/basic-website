f36fa3f15ee4a31cf466099e617e218316d763d8
Didi Medina <hi@didimedina.com>
2019-03-17 19:40:05 +0200

    added alert to test js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import anime from 'animejs';
 
+const button = document.querySelector('.alert');
+
+button.addEventListener('click', function(){alert('hello!')});
+
 anime({
     targets: '.anim-scale',
     scale: [0.8, 1],
@@ -8,3 +12,5 @@ anime({
     easing: 'easeOutQuad',
     duration: 800
 })
+
+
